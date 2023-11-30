a80f286fd730ad50e45331bc6c4067ba66fd20ca
Yusuf Orhan <yazilimcigenc2@gmail.com>
2023-11-30 13:45:42 +0300

    add new home viewmodel

--- app/src/main/java/com/yusuforhan/android/movies/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/yusuforhan/android/movies/presentation/home/HomeScreen.kt
@@ -1,22 +1,37 @@
 package com.yusuforhan.android.movies.presentation.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
@@ -52,10 +67,16 @@ fun HomeScreen(
 ) {
 
     Column {
+        Spacer(modifier = modifier.size(8.dp))
+
         ECProgressBar(state.isLoading)
         if (state.movies != null) {
             Spacer(modifier = modifier.padding(16.dp))
+            MovieSearchBar {searchString ->
+                println(searchString)
+            }
             state.movies.let { MovieList(movies = it, onItemClick = onItemClick) }
+
         }else if (state.error != null){
             Column(
                 verticalArrangement = Arrangement.Center,
@@ -131,6 +152,55 @@ fun ErrorScreen(
             tryButtonClick()
         }) {
             Text(text = "Try Again!")
+        }
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MovieSearchBar(
+    modifier : Modifier = Modifier,
+    hint: String = "Search",
+    onSearch: (String) -> Unit = {}
+) {
+    var text by remember {
+        mutableStateOf("")
+    }
+    var isHintDisplayed by remember {
+        mutableStateOf(hint != "")
+    }
+
+    Box(modifier = modifier) {
+        TextField(
+            value = text,
+            keyboardActions = KeyboardActions(onDone = {
+                onSearch(text)
+            }),
+            onValueChange = {
+                text =it
+            },
+            maxLines = 1,
+            singleLine = true,
+            textStyle = TextStyle(color = Color.Black),
+            shape = RoundedCornerShape(12.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color.White)
+            ,
+            modifier = Modifier
+                .fillMaxWidth()
+                .shadow(5.dp, CircleShape)
+                .background(Color.White, CircleShape)
+                .padding(horizontal = 20.dp)
+                .onFocusChanged {
+                    isHintDisplayed = it.isFocused != true && text.isEmpty()
+                }
+        )
+        if(isHintDisplayed) {
+            Text(
+                text = hint,
+                color = Color.LightGray,
+                modifier = Modifier
+                    .padding(horizontal = 20.dp, vertical = 12.dp)
+            )
         }
     }
 }
--- app/src/main/java/com/yusuforhan/android/movies/presentation/home/HomeScreenViewModel.kt
+++ app/src/main/java/com/yusuforhan/android/movies/presentation/home/HomeScreenViewModel.kt
@@ -1,42 +1,35 @@
 package com.yusuforhan.android.movies.presentation.home
 
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.produceState
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.yusuforhan.android.movies.common.Resource
 import com.yusuforhan.android.movies.data.model.Search
 import com.yusuforhan.android.movies.domain.repository.MoviesRepository
-import com.yusuforhan.android.movies.presentation.base.BaseViewModel
 import com.yusuforhan.android.movies.presentation.base.Event
-import com.yusuforhan.android.movies.presentation.base.State
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(
+class HomeScreenViewModel @Inject constructor(
     private val repository: MoviesRepository
-) : BaseViewModel<HomeUiState, HomeEvents>() {
+)  : ViewModel(){
+    val state = mutableStateOf(HomeState(isLoading = true))
 
 
-    val state = mutableStateOf(HomeUiState(isLoading = true))
-
-    override fun setInitialState(): HomeUiState = HomeUiState(true)
-
-    override fun handleEvent(event: HomeEvents) {
-        when (event) {
-            is HomeEvents.TryAgainClicked -> {
-                setState(getCurrentState().copy(true))
+    fun onEvent(event : HomeUiEvents){
+        when(event){
+            is HomeUiEvents.TryAgainClicked -> {
+                state.value = state.value.copy(isLoading = true)
                 getMovies()
+            }
+            is HomeUiEvents.OnSearch -> {
+                state.value = state.value.copy(isLoading = true)
+
             }
         }
     }
-
-    init {
-        getMovies()
-    }
-
     private fun getMovies() = viewModelScope.launch {
         when (val result = repository.getMovies()) {
             is Resource.Success -> {
@@ -50,18 +43,14 @@ class HomeViewModel @Inject constructor(
             }
         }
     }
-
-
 }
-
-data class HomeUiState(
+data class HomeState(
     val isLoading: Boolean = false,
     val movies: List<Search>? = emptyList(),
     val error: String? = null
-) : State
+)
 
-sealed class HomeEvents : Event {
-    data object TryAgainClicked : HomeEvents()
+sealed class HomeUiEvents {
+    data object TryAgainClicked : HomeUiEvents()
+    data class OnSearch(val searchString: String) : HomeUiEvents()
 }
-
-
